1232412d0a0d688f26f3a9171c0de89cf088e094
yanhenning <yanhenning@gmail.com>
2019-01-23 21:35:13 -0200

    add cliente tests

--- backendmercos/backend_mercos/tests/tests_cliente.py
+++ backendmercos/backend_mercos/tests/tests_cliente.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.urls import reverse,include, path
+from rest_framework import status
+from rest_framework.test import APITestCase, URLPatternsTestCase
+from ..models.cliente import Cliente
+from ..serializers import ClienteSerializer
 
-# Create your tests here.
+class ClienteTest(APITestCase):
+    def setUp(self):
+        Cliente.objects.create(id=1, nome="BB-8")
+        Cliente.objects.create(id=2, nome="R2-D2")
+
+    def test_dadoCliente_quandoBuscarId_entaoEncontrado(self):
+        url = reverse('cliente_id', kwargs={'pk':1})
+
+        serializer = ClienteSerializer(Cliente.objects.get(id=1))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data,serializer.data)
+
+    def test_dadoCliente_quandoBuscarId_entaoNaoEncontrado(self):
+        url = reverse('cliente_id', kwargs={'pk':99})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_dadoCliente_quandoCriarCliente_entaoClienteCriado(self):
+        url = reverse('cliente')
+        data = {
+        "nome": "C3-PO"
+    }
+        serializer = ClienteSerializer(Cliente.objects.get(id=1))
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Cliente.objects.get(nome=data['nome']).nome, data['nome'])
+
+    def test_dadoClientes_quandoBuscarTodos_entaoEncontrados(self):
+        url = reverse('cliente')
+
+        serializer = ClienteSerializer(Cliente.objects.all(), many=True)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data,serializer.data)
