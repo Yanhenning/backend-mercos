ce20f8da67a82f6fbc06a8f1b5f6ac9258b94b19
yan.henning <yanhenning@gmail.com>
2019-01-28 10:57:21 -0200

    changed settings file

--- backendmercos/settings.py
+++ backendmercos/settings.py
@@ -67,6 +67,8 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = False
+CORS_URLS_REGEX = r'^/api/.*$'
+SECURE_SSL_REDIRECT = True
 
 CORS_ORIGIN_WHITELIST = (
     'google.com',
@@ -79,7 +81,7 @@ CORS_ORIGIN_WHITELIST = (
 
 ROOT_URLCONF = 'backendmercos.urls'
 
-SECURE_SSL_REDIRECT = True
+
 
 TEMPLATES = [
     {
