b7c360a40f453d5b5cbee47ce48bf3ff4e672b2f
yanhenning <yanhenning@gmail.com>
2019-02-03 18:08:16 -0200

    added / to the end os urls endpoints

--- backend_mercos/urls.py
+++ backend_mercos/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     path('pedido/', views.Pedido.as_view(), name='pedido'),
     path('pedido/<int:id>/', views.PedidoById.as_view(),name='pedido_by_id'),
     path('produto/', views.Produto.as_view(), name='produto'),
-    path('produto/<int:id>', views.ProdutoById.as_view(), name='produto_by_id'),
+    path('produto/<int:id>/', views.ProdutoById.as_view(), name='produto_by_id'),
     path('cliente/', views.Cliente.as_view(), name='cliente'),
-    path('itempedido/<int:id>',views.ItemPedidoById.as_view(), name='itemPedido_by_id'),
-    path('cliente/<int:id>', views.ClienteById.as_view(), name='cliente_by_id'),
+    path('itempedido/<int:id>/',views.ItemPedidoById.as_view(), name='itemPedido_by_id'),
+    path('cliente/<int:id>/', views.ClienteById.as_view(), name='cliente_by_id'),
     path('usuario/<int:id>/pedido/', views.PedidoByIdUsuario.as_view(), name='pedidos_cliente'),
     path('pedido/<int:id>/itempedido/', views.ItemPedido.as_view(), name='item_pedido'),
 ]
